301c0a12081585cc34f670f6a0b21b7d51f28de2
Andrey Mazunin <97140182+js-easy-school@users.noreply.github.com>
2024-01-15 14:31:40 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@ let count = 0;
 
 window.addEventListener('devicemotion', function(event) {
   let acceleration = event.accelerationIncludingGravity;
-  let threshold = 0.5; // Порог для вертикального движения
+  let threshold = 0.25; // Порог для вертикального движения
 
   // Проверка на вертикальное движение
   if (Math.abs(acceleration.x) < threshold && Math.abs(acceleration.y) < threshold) {
