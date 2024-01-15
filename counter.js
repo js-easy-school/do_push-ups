ba70b9162133cb0cf8ce93ae66873850bbee8774
Andrey Mazunin <97140182+js-easy-school@users.noreply.github.com>
2024-01-15 14:33:44 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,12 +1,18 @@
 let count = 0;
+let isMoving = false; // Флаг для отслеживания движения
 
 window.addEventListener('devicemotion', function(event) {
   let acceleration = event.accelerationIncludingGravity;
-  let threshold = 0.25; // Порог для вертикального движения
+  let threshold = 0.5; // Порог для вертикального движения
 
   // Проверка на вертикальное движение
   if (Math.abs(acceleration.x) < threshold && Math.abs(acceleration.y) < threshold) {
-    count++;
-    document.getElementById('counter').innerText = count;
+    isMoving = false; // Устройство находится в покое
+  } else {
+    if (!isMoving) {
+      count++; // Устройство начало движение, увеличиваем счетчик
+      isMoving = true; // Устанавливаем флаг движения
+      document.getElementById('counter').innerText = count; // Обновляем отображение счетчика
+    }
   }
 });
